2a5b852d6639c81193210ded1a71d44e1cf0d837
prosper-20 <edwardprosper001@gmail.com>
2022-03-18 16:54:56 +0100

    updates

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import HomeView, RoomDetailView
+from .views import HomeView, RoomDetailView, contact
 from . import views
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path('room/<slug:slug>/', views.detail_view, name='detail')
+    path('room/<slug:slug>/', views.detail_view, name='detail'),
+    path("contact/", views.contact, name="contact")
 ]
